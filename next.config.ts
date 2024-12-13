f3e33c62bb5f633c7a4f93b7a0ec69593bed8de5
Zsolt <zsolt.deak01@gmail.com>
2024-12-13 19:55:31 +0200

    test6

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    webpackMemoryOptimizations: true,
+  },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
